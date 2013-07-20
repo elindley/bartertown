4e29e18d7277238788b098f071d7aa697b817db7
Evan Lindley <evan.lindley@gmail.com>
2013-07-20 00:39:54 -0700

    cleanup

--- dropdown.php
+++
@@ -1,9 +0,0 @@
-<select>
-<?php 
-	$query = "SELECT * FROM categories;";
-	$result = @mysql_query("$query");
-	while ($row = mysql_fetch_array($result)) {
-		echo"<option value=" . $row['id'] . ">" . $row['name'] . "</option>";
-	}
-?>
-</select>
