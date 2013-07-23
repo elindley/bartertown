db28bcf78617ae270f442d845c3003990e8914b5
jackaperkins <jackaperkins@gmail.com>
2013-07-23 01:38:17 -0700

    cleaning up project

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 include'inc/header.php';
 include'inc/db.php';
 ?>
-
-
-
-
 	<!-- ad titles -->
 	<div class="row text-center"><h2>Recent Posts</h2></div>
 	<div class="row">
 		<p><?php include'view.php'; ?> </p>
 	</div>
 	<br>
-
 <?php include'inc/footer.php'; ?>
