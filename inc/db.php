8d8eae6e862edf176e3906ba372a650326b52511
evan <evan.lindley@gmail.com>
2013-07-27 14:29:14 -0700

    add gitignore for db connection

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,5 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = 'owningtheotter';
-$database = 'bartertown';
+include 'secret.php';
 
 $connect = mysql_connect($host,$username,$password) or die('<p class="error">Unable to connect to the database server at this time.</p>');
 mysql_select_db($database,$connect) or die('<p class="error">Unable to connect to the database at this time.</p>');
