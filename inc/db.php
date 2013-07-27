a37e5268f3435df52399cea8efac56509f509259
evan <evan.lindley@gmail.com>
2013-07-27 14:18:19 -0700

    cleanup links

--- inc/db.php
+++ inc/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'root';
-$password = '';
+$password = 'owningtheotter';
 $database = 'bartertown';
 
 $connect = mysql_connect($host,$username,$password) or die('<p class="error">Unable to connect to the database server at this time.</p>');
--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,8 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Welcome to Bartertown</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/bartertown/css/bootstrap.css" rel="stylesheet" media="screen"></head>
-    <link href="/bartertown/css/custom.css" rel="stylesheet" media="screen"></head>
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen"></head>
+    <link href="css/custom.css" rel="stylesheet" media="screen"></head>
 
 <body>
 	<div class="container">
@@ -17,11 +17,11 @@
 
 	<!-- navigation -->
 	<div class="row">
-		<p class="span2"><a href="/bartertown/index.php">Home</a></p>
-		<p class="span2"><a href="/bartertown/search.php?cat=2">Bikes</a></p>
-		<p class="span2"><a href="/bartertown/search.php?cat=3">Cars</a></p>
-		<p class="span2"><a href="/bartertown/search.php?cat=4">Trucks</a></p>
-		<p class="span2"><a class="btn btn-primary" href="/bartertown/newad.php">Make a new ad</a>
+		<p class="span2"><a href="index.php">Home</a></p>
+		<p class="span2"><a href="search.php?cat=2">Bikes</a></p>
+		<p class="span2"><a href="search.php?cat=3">Cars</a></p>
+		<p class="span2"><a href="search.php?cat=4">Trucks</a></p>
+		<p class="span2"><a class="btn btn-primary" href="newad.php">Make a new ad</a>
 		<form class="span2" method ="get" action="searchbar.php" id="searchform">
 			<input type="text" class="span2" name="searchbar">
 		</form>
